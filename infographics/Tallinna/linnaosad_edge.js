12aa132de85d050d1c88d4076aba5307d829a3c7
malvarezap <malvarezap@gmail.com>
2015-10-15 10:16:23 +0300

    Revert "deleting"
    This reverts commit f4b7ee9782e787ac91daafb35d1bdddf86310743.

---
+++ infographics/Tallinna/linnaosad_edge.js
@@ -0,0 +1,81 @@
+
+(function(compId){var _=null,y=true,n=false,x11='break-word',x40='15px',x57='rgb(0, 0, 0)',x42='65px',e69='${RoundRect2}',x64='-234px',x100='103px',x25='183px',e85='${RoundRect3}',x114='Text5',x113='RoundRect5',d='display',x90='laane',x78='RoundRect3',e103='${Text4}',x6='rgba(202,0,25,1.00)',x21='rgba(255,255,255,1)',x27='ida',e49='${RoundRect}',e104='${laane}',x1='6.0.0',x66='196px',x43='lato, sans-serif',x88='191px',x60='222px',e86='${pohja}',x37='1',e118='${RoundRect5}',e119='${Text5}',x24='158px',x39='-196px',x20='rgba(49,45,41,1)',x89='119px',p='px',x31='-207px',x58='-248px',x53='106px',e87='${Text3}',x93='38px',x97='Text4',x33='227px',x94='221px',x54='171px',x36='RoundRect',x79='Text3',x83='89px',b='block',x63='Text2',x55='kesklinna',e71='${Text2}',x59='-6px',x3='6.0.0.400',lf='left',x67='66px',x28='0.37',x73='113px',x41='205px',e70='${kesklinna}',e48='${Text}',x106='-1px',x14='400',x107='170px',x77='105px',x38='rgba(49,45,41,1.00)',x19='17px',e50='${ida}',g='image',x112='77px',e117='${louna}',x65='9px',x81='11px',x22='6px',x32='5px',x115='182px',x35='5px 5px',x51='2px',x72='1px',x23='4px',x52='3px',x108='87px',m='rect',x74='pohja',x44='rgba(255,255,255,1.00)',e105='${RoundRect4}',x111='210px',x102='118px',x98='261px',i='none',x76='259px',x7='rgba(202,0,25,1)',x99='45px',x2='5.0.0',x109='louna',x92='246px',x4='rgba(0,0,0,0)',x95='84px',x34='80px',x26='auto',x10='Arial, Helvetica, sans-serif',l='normal',x12='nowrap',x96='RoundRect4',x47='true',x62='RoundRect2',x46='Text',x82='161px',x='text',x30='0px',o='opacity',x80='274px',x61='92px',x9='18',xc='rgba(0,0,0,1)';var g75='pohja.png',g17='logo.svg',g5='bg2.jpg',g56='kesklinna.png',g91='laane.png',g29='ida.png',g110='louna.png';var s84="<p style=\"margin: 0px;\">​<span style=\"font-weight: 700;\">Põhja piirkond</span></p><p style=\"margin: 0px;\"><span style=\"font-size: 12px;\">OÜ Jaaksoni Linnahooldus, aastane tööde maksumus 1,1 miljonit eurot.</span></p>",s45="<p style=\"margin: 0px;\">​<span style=\"font-weight: 700;\">Ida piirkond</span></p><p style=\"margin: 0px;\"><span style=\"font-size: 12px;\">OÜ N&amp;V, aastane tööde maksumus 2,6 miljonit eurot</span></p>",s68="<p style=\"margin: 0px;\">​<span style=\"font-weight: 700;\">Kesklinna piirkond </span><span style=\"font-size: 12px;\">Tänavapuhastuse AS, aastane tööde maksumus 3,6 miljonit eurot.</span></p>",s15="<p style=\"margin: 0px; line-height: 14px;\">​<span style=\"font-family: lato, sans-serif; font-style: italic;\">​</span><span style=\"font-family: lato, sans-serif; font-size: 11px; font-weight: 700; font-style: italic;\">Allikas:</span><span style=\"font-family: lato, sans-serif; font-size: 11px; font-style: italic;\"> pakkumuste edukaks tunnistamise protokollid</span></p><p style=\"margin: 0px; line-height: 14px;\"><span style=\"font-family: lato, sans-serif; font-size: 11px; font-style: italic;\">​</span><span style=\"font-family: lato, sans-serif; font-size: 11px; font-weight: 700; font-style: italic;\">Graafik: </span><span style=\"font-family: lato, sans-serif; font-size: 11px; font-style: italic;\">Miguel Álvarez</span></p>",s116="<p style=\"margin: 0px;\">​<span style=\"font-weight: 700;\">Lõuna piirkond</span> </p><p style=\"margin: 0px;\"><span style=\"font-size: 12px;\">OÜ N&amp;V, aastane tööde maksumus 825 000 eurot.</span></p>",s101="<p style=\"margin: 0px;\">​<span style=\"font-weight: 700;\">Lääne piirkond</span></p><p style=\"margin: 0px;\"><span style=\"font-size: 12px;\">OÜ Jaaksoni Linnahooldus, aastane tööde maksumus 1,3 miljonit eurot.</span></p>",s16="<p style=\"margin: 0px; line-height: 17px;\">​<span style=\"font-size: 15px; font-family: lato, sans-serif; color: rgb(202, 0, 25); font-weight: 700;\">OÜ Jaaksoni Linnahooldus</span><span style=\"font-size: 15px; font-family: lato, sans-serif;\">, juhatuse liige Urmas Teeorg, osanik JAAKSON &amp; KO AS kaudu Jüri Jaakson.</span></p>",s18="<p style=\"margin: 0px;\">​<span style=\"font-size: 15px; font-weight: 700;\">​</span><span style=\"font-size: 15px; font-family: lato, sans-serif; font-weight: 700; color: rgb(202, 0, 25);\">Tänavapuhastuse AS,</span><span style=\"font-size: 15px; font-family: lato, sans-serif;\"> juhatuse liige Vello Kink, aktsionärid Kartsoil OÜ kaudu Tõnis Karu ja Tõnu Karu (osalus 65,47%), Montes Music OÜ kaudu Vivo Tamm (osalus 20,24%) ning Ukava Consult OÜ kaudu Olev Kallas ja Vello Kink (osalus 14,28 %)</span></p>",s13="<p style=\"margin: 0px;\">​<span style=\"font-family: lato, sans-serif; font-size: 15px; font-weight: 700; color: rgb(202, 0, 25);\">OÜ N&amp;V</span><span style=\"font-family: lato, sans-serif; font-size: 15px;\">, juhatuse liige Marika Korsten, osanik Nikolai Ossipenko</span></p>",s8="<p style=\"margin: 0px;\">​<span style=\"font-family: alfa-slab-one, serif;\">Ettevõtete juhid ja omanikud</span></p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={'lato, sans-serif':'<script src=\"http://use.edgefonts.net/lato:n9,i4,n1,i7,i9,n7,i1,i3,n4,n3:all.js\"></script>','alfa-slab-one, serif':'<script src=\"http://use.edgefonts.net/alfa-slab-one:n4:all.js\"></script>'},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'bg2',t:g,r:['-1px','0px','676px','833px','auto','auto'],f:[x4,im+g5,'0px','0px','98.8%','98.8%','no-repeat']},{id:'dot1',t:'ellipse',r:['18px','610px','12px','12px','auto','auto'],br:["50%","50%","50%","50%"],f:[x6],s:[0,"rgb(0, 0, 0)",i]},{id:'dot2',t:'ellipse',r:['18px','643px','12px','12px','auto','auto'],br:["50%","50%","50%","50%"],f:[x7],s:[0,"rgb(0, 0, 0)",i]},{id:'dot3',t:'ellipse',r:['18px','711px','12px','12px','auto','auto'],br:["50%","50%","50%","50%"],f:[x7],s:[0,"rgb(0, 0, 0)",i]},{id:'ida_sym',symbolName:'ida_sym',t:m,r:['362','151','158','183','auto','auto']},{id:'kesk_sym',symbolName:'kesk_sym',t:m,r:['301px','242px','106','171','auto','auto']},{id:'pohja_sym',symbolName:'pohja_sym',t:m,r:['138','181','196','113','auto','auto']},{id:'laane_sym',symbolName:'laane_sym',t:m,r:['121','256','191','118','auto','auto']},{id:'louna_sym',symbolName:'louna_sym',t:m,r:['184px','367px','undefined','undefined','auto','auto']},{id:'Subtitle',t:x,r:['15px','561px','auto','auto','auto','auto'],text:s8,n:[x10,[x9,p],"rgba(49,45,42,1.00)",l,i,"",x11,x12]},{id:'Text1',t:x,r:['47px','606px','446px','21px','auto','auto'],text:s13,align:"left",n:[x10,[x9,p],"rgba(49,45,42,1)",x14,i,l,x11,l],ts:["","","","",i]},{id:'Text6',t:x,r:['18px','769px','278px','36px','auto','auto'],text:s15,align:"left",n:[x10,[x9,p],"rgba(49,45,42,1)",x14,i,l,x11,l],ts:["","","","",i]},{id:'Text3',t:x,r:['47px','707px','599px','36px','auto','auto'],text:s16,align:"left",n:[x10,[x9,p],"rgba(49,45,42,1)",x14,i,l,x11,l],ts:["","","","",i]},{id:'logo',t:g,r:['530px','764px','121px','44px','auto','auto'],f:[x4,im+g17,'0px','0px']},{id:'Text2',t:x,r:['47px','640px','590px','51px','auto','auto'],text:s18,align:"left",n:[x10,[x9,p],"rgba(49,45,42,1)",x14,i,l,x11,l],ts:["","",x19,"",i]},{id:'NV',t:m,r:['16px','606px','92px','21px','auto','auto'],o:'0',f:[x20],s:[0,"rgb(0, 0, 0)",i]},{id:'Tanava',t:m,r:['16px','638px','170px','21px','auto','auto'],o:'0',f:[x20],s:[0,"rgb(0, 0, 0)",i]},{id:'Jaaksoni',t:m,r:['16px','705px','211px','21px','auto','auto'],o:'0',f:[x20],s:[0,"rgb(0, 0, 0)",i]}],style:{'${Stage}':{isStage:true,r:['null','null','660px','813px','auto','auto'],overflow:'visible',f:[x21]}}},tt:{d:500,a:n,data:[]}},"ida_sym":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x22,x23,x24,x25,x26,x26],id:x27,o:x28,t:g,f:[x4,im+g29,x30,x30]},{s:[0,xc,i],r:[x31,x32,x33,x34,x26,x26],br:[x32,x32,x32,x35],t:m,id:x36,o:x37,v:i,f:[x38]},{r:[x39,x40,x41,x42,x26,x26],n:[x43,[14,p],x44,l,i,'',x11,l],text:s45,id:x46,o:x37,v:i,t:x}],style:{'${symbolSelector}':{isStage:x47,r:[undefined,undefined,x24,x25]}}},tt:{d:500,a:y,l:{"no_ida":0,"si_ida":500},data:[["eid55",d,0,0,"linear",e48,i,i],["eid57",d,500,0,"linear",e48,i,b],["eid21",o,0,0,"linear",e49,'0','0'],["eid19",o,500,0,"linear",e49,'0','1'],["eid3",o,0,0,"linear",e50,'0.000000','0.000000'],["eid4",o,500,0,"linear",e50,'0.000000','0.37'],["eid56",d,0,0,"linear",e49,i,i],["eid58",d,500,0,"linear",e49,i,b],["eid22",o,0,0,"linear",e48,'0','0'],["eid20",o,500,0,"linear",e48,'0','1']]}},"kesk_sym":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x51,x52,x53,x54,x26,x26],id:x55,o:x28,t:g,f:[x4,im+g56,x30,x30]},{s:[0,x57,i],r:[x58,x59,x60,x61,x26,x26],br:[x32,x32,x32,x35],t:m,id:x62,o:x37,v:i,f:[x20]},{t:x,id:x63,o:x37,r:[x64,x65,x66,x67,x26,x26],ts:['','','','',i],v:i,n:[x43,[14,p],x21,x14,i,l,x11,l],align:lf,text:s68}],style:{'${symbolSelector}':{isStage:x47,r:[undefined,undefined,x53,x54]}}},tt:{d:500,a:y,l:{"no_kesk":500,"si_kesk":500},data:[["eid24",o,0,0,"linear",e69,'0','0'],["eid26",o,500,0,"linear",e69,'0','1'],["eid54",d,0,0,"linear",e69,i,i],["eid52",d,500,0,"linear",e69,i,b],["eid8",o,0,0,"linear",e70,'0','0'],["eid7",o,500,0,"linear",e70,'0','0.37'],["eid53",d,0,0,"linear",e71,i,i],["eid51",d,500,0,"linear",e71,i,b],["eid23",o,0,0,"linear",e71,'0','0'],["eid25",o,500,0,"linear",e71,'0','1']]}},"pohja_sym":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x72,x52,x66,x73,x26,x26],id:x74,o:x28,t:g,f:[x4,im+g75,x30,x30]},{s:[0,x57,i],r:[x76,x72,x66,x77,x26,x26],br:[x32,x32,x32,x35],t:m,id:x78,o:x37,v:i,f:[x20]},{t:x,id:x79,o:x37,r:[x80,x81,x82,x83,x26,x26],ts:['','','','',i],v:i,n:[x43,[14,p],x21,x14,i,l,x11,l],align:lf,text:s84}],style:{'${symbolSelector}':{isStage:x47,r:[undefined,undefined,x66,x73]}}},tt:{d:500,a:y,l:{"no_pohja":0,"si_pohja":500},data:[["eid28",o,0,0,"linear",e85,'0','0'],["eid30",o,500,0,"linear",e85,'0','1'],["eid10",o,0,0,"linear",e86,'0','0'],["eid9",o,500,0,"linear",e86,'0','0.37'],["eid27",o,0,0,"linear",e87,'0','0'],["eid29",o,500,0,"linear",e87,'0','1'],["eid41",d,0,0,"linear",e87,i,i],["eid40",d,500,0,"linear",e87,i,b],["eid42",d,0,0,"linear",e85,i,i],["eid39",d,500,0,"linear",e85,i,b]]}},"laane_sym":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x72,x52,x88,x89,x26,x26],id:x90,o:x28,t:g,f:[x4,im+g91,x30,x30]},{s:[0,x57,i],r:[x92,x93,x94,x95,x26,x26],br:[x32,x32,x32,x35],t:m,id:x96,o:x37,v:i,f:[x20]},{t:x,id:x97,o:x37,r:[x98,x99,x88,x100,x26,x26],ts:['','','','',i],v:i,n:[x43,[14,p],x21,x14,i,l,x11,l],align:lf,text:s101}],style:{'${symbolSelector}':{isStage:x47,r:[undefined,undefined,x88,x102]}}},tt:{d:500,a:y,l:{"no_laane":0,"si_laane":500},data:[["eid47",d,0,0,"linear",e103,i,i],["eid49",d,500,0,"linear",e103,i,b],["eid31",o,0,0,"linear",e103,'0','0'],["eid33",o,500,0,"linear",e103,'0','1'],["eid12",o,0,0,"linear",e104,'0','0'],["eid11",o,500,0,"linear",e104,'0','0.37'],["eid32",o,0,0,"linear",e105,'0','0'],["eid34",o,500,0,"linear",e105,'0','1'],["eid48",d,0,0,"linear",e105,i,i],["eid50",d,500,0,"linear",e105,i,b]]}},"louna_sym":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x106,x32,x107,x108,x26,x26],id:x109,o:x28,t:g,f:[x4,im+g110,x30,x30]},{s:[0,x57,i],r:[x111,x106,x107,x112,x26,x26],br:[x32,x32,x32,x35],t:m,id:x113,o:x37,v:i,f:[x20]},{t:x,id:x114,o:x37,r:[x94,x32,x115,x108,x26,x26],ts:['','','','',i],v:i,n:[x43,[14,p],x21,x14,i,l,x11,l],align:lf,text:s116}],style:{'${symbolSelector}':{isStage:x47,r:[undefined,undefined,x107,x108]}}},tt:{d:490,a:y,l:{"no_louna":0,"si_louna":490},data:[["eid17",o,0,0,"linear",e117,'0','0'],["eid18",o,490,0,"linear",e117,'0','0.37'],["eid46",d,0,0,"linear",e118,i,i],["eid44",d,490,0,"linear",e118,i,b],["eid36",o,0,0,"linear",e118,'0','0'],["eid38",o,490,0,"linear",e118,'0','1'],["eid35",o,0,0,"linear",e119,'0','0'],["eid37",o,490,0,"linear",e119,'0','1'],["eid45",d,0,0,"linear",e119,i,i],["eid43",d,490,0,"linear",e119,i,b]]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-280027541");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${laane_sym}","mouseover",function(sym,e){sym.getSymbol("laane_sym").play(1000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${laane_sym}","mouseout",function(sym,e){sym.getSymbol("laane_sym").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${pohja_sym}","mouseover",function(sym,e){sym.getSymbol("pohja_sym").play(1000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${pohja_sym}","mouseout",function(sym,e){sym.getSymbol("pohja_sym").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${kesk_sym}","mouseover",function(sym,e){sym.getSymbol("kesk_sym").play(1000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${kesk_sym}","mouseout",function(sym,e){sym.getSymbol("kesk_sym").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${ida_sym}","mouseover",function(sym,e){sym.getSymbol("ida_sym").play(1000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${ida_sym}","mouseout",function(sym,e){sym.getSymbol("ida_sym").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${louna_sym}","mouseover",function(sym,e){sym.getSymbol("louna_sym").play(1000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${louna_sym}","mouseout",function(sym,e){sym.getSymbol("louna_sym").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${NV}","mouseover",function(sym,e){sym.getSymbol("ida_sym").play(1000);sym.getSymbol("louna_sym").play(1000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${NV}","mouseout",function(sym,e){sym.getSymbol("ida_sym").play(0);sym.getSymbol("louna_sym").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Tanava}","mouseover",function(sym,e){sym.getSymbol("kesk_sym").play(1000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Tanava}","mouseout",function(sym,e){sym.getSymbol("kesk_sym").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Jaaksoni}","mouseover",function(sym,e){sym.getSymbol("pohja_sym").play(1000);sym.getSymbol("laane_sym").play(1000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Jaaksoni}","mouseout",function(sym,e){sym.getSymbol("pohja_sym").play(0);sym.getSymbol("laane_sym").play(0);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'ida_sym'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",250,function(sym,e){sym.stop();});
+//Edge binding end
+})("ida_sym");
+//Edge symbol end:'ida_sym'
+
+//=========================================================
+
+//Edge symbol: 'kesk_sym'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",250,function(sym,e){sym.stop();});
+//Edge binding end
+})("kesk_sym");
+//Edge symbol end:'kesk_sym'
+
+//=========================================================
+
+//Edge symbol: 'pohja_sym'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",250,function(sym,e){sym.stop();});
+//Edge binding end
+})("pohja_sym");
+//Edge symbol end:'pohja_sym'
+
+//=========================================================
+
+//Edge symbol: 'laane_sym'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",250,function(sym,e){sym.stop();});
+//Edge binding end
+})("laane_sym");
+//Edge symbol end:'laane_sym'
+
+//=========================================================
+
+//Edge symbol: 'louna_sym'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",250,function(sym,e){sym.stop();});
+//Edge binding end
+})("louna_sym");
+//Edge symbol end:'louna_sym'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-280027541");
